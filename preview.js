4d7408792160beec082a7d206d6e298307bb87bf
Nobushige-T <Ponzu_1231@yahoo.co.jp>
2019-07-28 23:52:59 +0900

    不要ファイル削除

--- preview.js
+++
@@ -1,8 +0,0 @@
-function MoveCheck() {
-    var res = confirm("本当に削除しますか？");
-    if (res == true) {
-        window.location.href = "delete.php";
-    } else {
-        alert("削除を取り消しました。");
-    }
-}
